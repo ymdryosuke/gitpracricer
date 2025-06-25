3a5d757df3034086856cbdd23aa6fe53643746a1
Yamada Ryousuke <cnt14148@ict.nitech.ac.jp>
2025-06-26 02:45:47 +0900

    Initial commit: Add main.c, README.md, and .gitignore

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void del_digit(char str[])
+{
+	/* ここにコードを追加 */
+    int i = 0, j = 0;
+    while (str[i] != '\0') {       // 文字列の終端までループ
+        if (str[i] < '0' || str[i] > '9') { // 数字でない場合
+            str[j++] = str[i];     // 数字でない文字を前に詰める
+        }
+        i++;
+    }
+    str[j] = '\0'; // 新しい終端を設定
+    
+}
+
+int main(void)
+{
+	char str[100];
+	printf("文字列を入力してください（数字含んでOK）：str = ");
+	scanf("%s",str);
+	
+	printf("数字の削除！\n");
+	
+	del_digit(str);
+	
+	printf("str = %s\n",str);
+	
+	return(0);
+}
